e82320afc1f5b549945ee3c59208d14424885eb5
Tamys Guimarães <tamys.guimaraes@gmail.com>
2020-06-13 20:25:34 -0300

    Aula finalizada

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// // // var nome = "Tamys";
+// // // var idade = 32;
+// // // //alert(nome +" tem " + idade);
+// // // console.log(nome);
+// // // console.log(idade);
+// // var lista = ["maçã", "banana", "laranja"];
+// // console.log(lista[2]);
+// // lista.push("uva");
+// // console.log(lista.length);
+// // //console.log(lista.reverse);
+// // console.log(lista.join(";"));
+
+// var fruta = { nome: "maçã", cor: "vermelha" };
+// console.log(fruta.nome);
+// console.log(fruta.cor);
+
+// var frutas = [{ nome: "maçã", cor: "vermelha" }, { nome: "morango", cor: "vermelha" }];
+// console.log(frutas[1].nome);
+
+//var idade = 18;
+// var idade = prompt("Idade");
+// if (idade >= 18) {
+//     alert("já pode beber");
+// } else {
+//     alert("não pode beber");
+//}
+
+// var count;
+// //while (count <= 5) {
+// for (count = 0; count <= 5; count++)
+//     console.log(count);
+// count++;
+// //}
+// var d = new Date();
+// console.log(d.getMonth() + 1);
+var var1 = "simplicity...";
+var var2 = "Leonardo";
+console.log(var1 + var2 + 1425 + "-" + 1519);
